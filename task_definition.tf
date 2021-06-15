46038a86fe88614e54dcdadc1ca9565e310d5143
root <root@ip-172-31-49-203.ec2.internal>
2021-06-15 07:14:46 +0000

    bugfix

--- task_definition.tf
+++ task_definition.tf
@@ -1,4 +1,4 @@
 resource "aws_ecs_task_definition" "task_definition" {
   family = "worker"
-  container_definitions =  = file("container-definitions/container-def.json")
+  container_definitions = file("container-definitions/container-def.json")
 }
